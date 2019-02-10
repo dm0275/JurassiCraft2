80a14f720920120c2c6661ad4fd9c34a0dfbbcc1
PT400C <pt400c@gmail.com>
2019-02-10 19:48:36 +0100

    Upped versions for the next release

--- src/main/java/org/jurassicraft/JurassiCraft.java
+++ src/main/java/org/jurassicraft/JurassiCraft.java
@@ -20,9 +20,9 @@ import org.jurassicraft.server.proxy.ServerProxy;
 public class JurassiCraft {
     public static final String MODID = "jurassicraft";
     public static final String NAME = "JurassiCraft";
-    public static final String VERSION = "2.1.20 ";
+    public static final String VERSION = "2.1.21 ";
 
-    public static final String LLIBRARY_VERSION = "1.7.14";
+    public static final String LLIBRARY_VERSION = "1.7.15";
     @SidedProxy(serverSide = "org.jurassicraft.server.proxy.ServerProxy", clientSide = "org.jurassicraft.client.proxy.ClientProxy")
     public static ServerProxy PROXY;
 
